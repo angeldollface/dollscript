b12d3fa3dbdb2b66b1b4b59d4bb3ba6e14963bdb
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2024-05-24 17:18:43 +0200

    Update lexer.rs

--- src/modules/lexer.rs
+++ src/modules/lexer.rs
@@ -1,3 +1,12 @@
+/*
+THE DOLLSCRIPT INTERPRETER 
+by Alexander Abraham 
+a.k.a. "Angel Dollface".
+Licensed under the DSL v1.
+*/
+
+/// All the types of tokens that
+/// "Dollscript" knows.
 pub enum TokenType {
     InspoImport,
     SlayyPublic,
